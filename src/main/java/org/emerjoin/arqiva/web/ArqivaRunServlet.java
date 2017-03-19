7fc70199536b1acf8895b0c8905221c4d68327e4
mjunior <francisco.junior.mario@gmail.com>
2017-03-19 19:43:26 +0200

    Initial code commit

---
+++ src/main/java/org/emerjoin/arqiva/web/ArqivaRunServlet.java
@@ -0,0 +1,94 @@
+package org.emerjoin.arqiva.web;
+
+import org.emerjoin.arqiva.core.Arqiva;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Mário Júnior
+ */
+@WebServlet(name = "ArqivaRun",urlPatterns = "/*",loadOnStartup = 1)
+public class ArqivaRunServlet extends HttpServlet {
+
+    private Arqiva arqivaInstance = null;
+    private static Logger log = LoggerFactory.getLogger(ArqivaRunServlet.class);
+
+    public ArqivaRunServlet(){ }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        if(Middleware.INVALIDATE_TOPICS_TREE)
+            Middleware.ARQIVA_PROJECT.invalidateTopicsTree();
+
+        String resourceRequested = req.getRequestURI().substring(req.getContextPath().length()+1,req.getRequestURI().length());
+
+        log.debug("Resource requested: "+resourceRequested);
+        if(resourceRequested.equals("index.html")){
+            renderIndexPage(req,resp);
+            return;
+        }
+
+        if(resourceRequested.startsWith("topics/"))
+            renderTopicPage(resourceRequested,req,resp);
+        else resp.sendError(404);
+
+    }
+
+    private void renderIndexPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+       String templatePageHtml =  arqivaInstance.renderIndexPage();
+       writeHtml(templatePageHtml,resp);
+
+
+    }
+
+
+    private void renderTopicPage(String topic, HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        emitHeaders(resp);
+        String topicPageHtml = arqivaInstance.renderTopicPage(topic.replace(".html","").replace("topics/",""));
+        writeHtml(topicPageHtml,resp);
+
+
+    }
+
+    public void init(){
+
+        log.info("Initializing ArqivaRunServlet...");
+
+        arqivaInstance = new Arqiva(Middleware.ARQIVA_PROJECT);
+        log.info("Preparing Arqiva instance...");
+        arqivaInstance.getReady();
+
+    }
+
+    private void emitHeaders(HttpServletResponse response) throws ServletException,IOException{
+
+        response.setContentType("text/html");
+
+    }
+
+    private void writeHtml(String html, HttpServletResponse response) throws ServletException, IOException {
+
+        emitHeaders(response);
+        //log("Writing "+html.length()+" characters out");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.print(html);
+        printWriter.flush();
+        printWriter.close();
+
+    }
+
+}
---
+++ src/main/java/org/emerjoin/arqiva/web/Middleware.java
@@ -0,0 +1,15 @@
+package org.emerjoin.arqiva.web;
+
+import org.emerjoin.arqiva.core.Arqiva;
+import org.emerjoin.arqiva.core.ArqivaProject;
+import org.emerjoin.arqiva.core.Project;
+
+/**
+ * @author Mário Júnior
+ */
+public class Middleware {
+
+    public static boolean INVALIDATE_TOPICS_TREE = false;
+    public static Project ARQIVA_PROJECT = null;
+
+}
